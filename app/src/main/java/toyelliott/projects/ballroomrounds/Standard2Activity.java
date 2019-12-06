eb672d4cbf87016589202d4d6c61d7843fd230cb
Elliott <elliott_toy@yahoo.com>
2019-12-05 22:46:36 -0800

    Made a 2nd standard

---
+++ app/src/main/java/toyelliott/projects/ballroomrounds/Standard2Activity.java
@@ -0,0 +1,14 @@
+package toyelliott.projects.ballroomrounds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Standard2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standard2);
+    }
+}
