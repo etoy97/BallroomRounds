645c6e6a8dd92ba1a0c95745a088d055267aa874
etoy97 <Elliott_Toy@yahoo.com>
2020-06-05 14:28:16 -0700

    Update YouTubeConfig.java

--- app/src/main/java/toyelliott/projects/ballroomrounds/YouTubeConfig.java
+++ app/src/main/java/toyelliott/projects/ballroomrounds/YouTubeConfig.java
@@ -4,7 +4,7 @@ public class YouTubeConfig {
     public YouTubeConfig() {
 
     }
-    private static final String API_KEY = "AIzaSyDozhlwsjdJcr4ovoG9mQWh_VVHoAjfWrE";
+    private static final String API_KEY = "PersonalKey";
 
     public static String getAPI_KEY() {
         return API_KEY;
